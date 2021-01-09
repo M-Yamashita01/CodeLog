87364dac6caaf29e03d28cb31a9a1302b899f891
Masato Yamashita <4653960+M-Yamashita01@users.noreply.github.com>
2021-01-10 07:24:39 +0900

    Set enableForwardingHttpRequest as true in host.json. And add hello directory to display hello world by get request.

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
-curl -LO https://portableruby.s3.amazonaws.com/ruby-2.6.3.zip && \
-unzip ruby-2.6.3.zip -d portableruby && \
-rm ruby-2.6.3.zip
+# curl -LO https://portableruby.s3.amazonaws.com/ruby-2.6.3.zip && \
+# unzip ruby-2.6.3.zip -d portableruby && \
+# rm ruby-2.6.3.zip
+brew update && \
+brew install rbenv && \
+rbenv install 2.7.0 && \
+rbenv local 2.7.0
