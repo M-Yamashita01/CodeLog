8fee90d8343343773f299a56ec49a2196a8e4a7d
Masato Yamashita <4653960+M-Yamashita01@users.noreply.github.com>
2021-01-14 03:57:41 +0900

    Remove files to use Rails instead of Azure Functions.

--- install.sh
+++
@@ -1,6 +0,0 @@
-# https://portableruby.com/
-# Portable, self-contained Ruby binaries that can run on any distribution of Linux x86-64.
-
-curl -LO https://portableruby.s3.amazonaws.com/ruby-2.6.3.zip && \
-unzip ruby-2.6.3.zip -d portableruby && \
-rm ruby-2.6.3.zip
