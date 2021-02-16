1c9ca8d8624e39259984140916430152ad3c40d9
Masato Yamashita <4653960+M-Yamashita01@users.noreply.github.com>
2021-02-16 23:24:40 +0900

    Add git in dockerfile. Add git clone statement.

--- server/app/controllers/code_log_controller.rb
+++ server/app/controllers/code_log_controller.rb
@@ -3,6 +3,8 @@ class CodeLogController < ApplicationController
   end
 
   def get_repository
-    logger.info 'get_repository log'
+    system('mkdir repository')
+    system('git clone https://github.com/M-Yamashita01/CodeLog.git repository')
+    puts 'Finished clone repository.'
   end
 end
