a1e1e94edc3d3f0b02b3f25b89f1642c0753285f
Masato Yamashita <4653960+M-Yamashita01@users.noreply.github.com>
2021-05-04 22:30:24 +0900

    Create repository path when checkout repository button, and Display respository name list.

--- server/app/controllers/code_log_controller.rb
+++ server/app/controllers/code_log_controller.rb
@@ -6,18 +6,22 @@ class CodeLogController < ApplicationController
     home_path = Rails.root
     # File.dirname(home_path + '/'+ 'repository')
     repository_path = home_path.to_s + '/'+ 'repository/'
-    Dir.glob(repository_path + '*') do |f|
-      puts '----directory-------'
-      puts f.to_s
-    end
+    repository_path = Rails.root.join('repository', '*', '*')
+    @repository_list = Dir.glob(repository_path)
   end
 
   def get_repository
     repository_name = params[:repository]
+    repository_name_array = repository_name.split('/')
+    project_name = repository_name_array.last
+    organization_name = repository_name_array[repository_name_array.length - 2]
 
-    create_repository_dir_cmd = 'mkdir -p repository/code_log'
+    logger.info("organization name: #{organization_name}, project name: #{project_name}")
+
+    checkout_path = "repository/#{organization_name}/#{project_name}"
+    create_repository_dir_cmd = "mkdir -p #{checkout_path}"
     execute_cmd(create_repository_dir_cmd)
-    clone_repository_cmd = "git clone #{repository_name} repository"
+    clone_repository_cmd = "git clone #{repository_name} #{checkout_path}"
     execute_cmd(clone_repository_cmd)
 
     redirect_to('/code_log/home')
