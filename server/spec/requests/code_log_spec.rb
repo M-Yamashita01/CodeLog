2822e27483a27d772fb32a8c5a2818a17e62d0cd
Masato Yamashita <4653960+M-Yamashita01@users.noreply.github.com>
2021-05-08 07:00:06 +0900

    Add rspec gem in Gemfile. And create sample rspec file.

---
+++ server/spec/requests/code_log_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe "CodeLogs", type: :request do
+  describe "GET /index" do
+    pending "add some examples (or delete) #{__FILE__}"
+  end
+
+  describe "#get_git_log" do
+    it 'test' do
+      expect('test').to eq 'test'
+    end
+  end
+end
